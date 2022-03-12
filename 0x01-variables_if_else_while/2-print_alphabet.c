c6e0de9201bedf23a77e352c7700fa9fc2b814c9
Olaoluwa-19 <ayegbusiolaoluwa98@gmail.com>
2022-03-12 13:37:06 -0800

    printing alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: the main function prints the alphabet in lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
