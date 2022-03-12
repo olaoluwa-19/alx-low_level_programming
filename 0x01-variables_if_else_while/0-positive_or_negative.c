246462346901b4b5e554ce8389a8433770841035
Olaoluwa-19 <ayegbusiolaoluwa98@gmail.com>
2022-03-12 11:46:09 -0800

    assigning numbers to a variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,10 @@
 
 /**
  * main - main function
- *  
+ *
  * Description: the main function
  * Puts text to stout
- * But you're totally free to use several lines to properly 
+ * But you're totally free to use several lines to properly
  * Return: 0
  */
 int main(void)
