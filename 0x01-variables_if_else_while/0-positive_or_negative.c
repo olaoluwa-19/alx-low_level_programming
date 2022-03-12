b510cc36a9744f6dcca49ce56f2ebbfc64ab82d3
Olaoluwa-19 <ayegbusiolaoluwa98@gmail.com>
2022-03-12 11:09:10 -0800

    assigning numbers to a variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 
 /**
- * main assigns a random number to int n everytime
- * it executes it, and prints it
- * Return: Always 0 (Success)
+ * main - main function
+ * 
+ * Description: the main assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: 0
  */
 int main(void)
 {
