2a546ae34b51827c3031f56bc55062c60744f69e
Olaoluwa-19 <ayegbusiolaoluwa98@gmail.com>
2022-03-13 13:50:50 -0700

    printing single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: the main function prints all single digit numbers of base 10
+ * followed by a new line
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+		printf("%d%\n", n);
+		return (0);
+}
