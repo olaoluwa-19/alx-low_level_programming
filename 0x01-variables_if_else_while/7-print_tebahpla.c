f161ceea532e24046c3b2b59bbf4a745ec4b6fa5
Olaoluwa-19 <ayegbusiolaoluwa98@gmail.com>
2022-03-13 15:27:07 -0700

    printing alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/***
+ * main - main function
+ * Description: the main function prints the lowercase alphabet
+ * in reverse, followed by a new line
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
