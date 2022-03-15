43022a34e717d64d7bc2e5ebf2c7223b48a28e16
Olaoluwa-19 <ayegbusiolaoluwa98@gmail.com>
2022-03-15 09:37:19 -0700

    printing _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@
  * for all functions used in nested_loops directories
 */
 int _putchar(char c);
-void print alphabet(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
