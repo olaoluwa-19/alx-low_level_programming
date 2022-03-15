07b3630dba3d15e9df525832e20c60346551e42d
Olaoluwa-19 <ayegbusiolaoluwa98@gmail.com>
2022-03-15 10:50:14 -0700

    printing alphabets in lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: header file contains all functions fir nested loops
+*/
+
 int _putchar(char c);
 void print_alphabet(void);
+
+#endif
